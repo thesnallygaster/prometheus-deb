5daec29099eca3f3d9474e971ce21892d673becc
Damian Duży <dame@zakonfeniksa.org>
2025-10-04 15:51:19 +0200

    Update npm version to 10.9.4

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ ARCHITECTURE="$(dpkg --print-architecture)"
 
 GO_VERSION="1.25.1"
 NODE_VERSION="22.20.0"
-NPM_VERSION="9.9.4"
+NPM_VERSION="10.9.4"
 
 if [ "${DISTRO}" == "debian" ]; then
 cat << EOF > /etc/apt/sources.list.d/debian.sources
